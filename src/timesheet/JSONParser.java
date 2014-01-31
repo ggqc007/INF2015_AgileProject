26584f2d36d69c854160f7b2cc763c949f51b3de
Guillaume Gagnon <ggqc007@gmail.com>
2014-01-31 13:12:13 -0500

    Premiere ebauche de JSONParser  .... j'ai encore du travail à faire mais je laisse Thomas mettre sa partie avant

--- src/timesheet/JSONParser.java
+++ src/timesheet/JSONParser.java
@@ -10,18 +10,70 @@
  *   Thomas Robert de Massy
  *
  */
-
 package timesheet;
-
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
-
 public class JSONParser {
 
-    public JSONParser() {
-        
+    private static final String NOM_CHAMP_NUMERO_EMPLOYE = "numero_employe";
+    private static final String NOM_CHAMP_PROJET = "projet";
+    private static final String NOM_CHAMP_MINUTES = "minutes";
+    private static final String[] REPLACE_KEYS = {"weekend1", "weekend2"};
+    private static final String[] REPLACEMENT_KEYS = {"jour6", "jour7"};
+    private static int replaceCount;
+
+    public static TimeSheet toTimeSheet(JSONObject json) {
+
+        TimeSheet timesheet = new TimeSheet();
+        JSONArray jsonElements = json.names();
+
+        for (int i = 0; i < jsonElements.size(); i++) {
+            if (jsonElements.get(i).equals(NOM_CHAMP_NUMERO_EMPLOYE)) {
+                timesheet.setUserId(json.getInt(jsonElements.getString(i)));
+            } else {
+                Day newDay = timesheet.addDay(jsonElements.getString(i));
+                JSONArray day = json.getJSONArray(jsonElements.getString(i));
+
+                if (!day.isEmpty()) {
+                    for (int j = 0; j < day.size(); j++) {
+                        JSONObject taskObj = day.getJSONObject(j);
+                        // Lire le nom du jour ici
+                        if (!taskObj.isNullObject()) {
+                            newDay.addTask(taskObj.getInt(NOM_CHAMP_PROJET), taskObj.getInt(NOM_CHAMP_MINUTES));
+                        }
+                    }
+                }
+            }
+        }
+        return timesheet;
     }
-    
+
+    public static String format(String theString) {
+        // creation instance de timesheet ici??
+        return replaceKeys(theString);
+    }
+
+    public static String reportToJSONText(JSONObject JSONObj) {
+        // est-ce plutot un array dans l'enonce???
+        return JSONObj.toString();
+    }
+
+    /**
+     * Retourne une nouvelle String apres avoir fait le remplacement des
+     * elements du tableau REPLACE_KEYS par ceux de REPLACEMENT_KEYS dans
+     * l'ordre.
+     */
+    private static String replaceKeys(String theString) {
+        replaceCount = 0;
+        for (String REPLACE_KEY : REPLACE_KEYS) {
+            if (theString.contains(REPLACE_KEY)) {
+                theString = theString.replace(REPLACE_KEY, REPLACEMENT_KEYS[replaceCount]);
+                replaceCount++;
+            }
+        }
+        return theString;
+    }
+
 }
