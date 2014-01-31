ffb81ff01b4ae24665962f691e7e835020ea427a
Thomas Robert de Massy <robert_de_massy.thomas@courrier.uqam.ca>
2014-01-31 17:33:35 -0500

    Méthodes de l'objet Employe

--- src/timesheet/Employe.java
+++ src/timesheet/Employe.java
@@ -18,10 +18,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * Objet Employe
+ * 
+ */
 public class Employe {
 
+    private int                       employeId;
     private final List<TimeSheetData> timesheets;
     
+    
+    /**
+     * Constructeur de Employe.
+     */
     public Employe() {
         
         TimeSheetData timesheet = new TimeSheetData();
@@ -31,5 +40,85 @@ public class Employe {
         timesheets.add(timesheet);
         
     }
+
+    
+    /**
+     * Constructeur avec paramètres de Employe.
+     * 
+     * @param id Numéro d'identification de l'employé.
+     */
+    public Employe(int id) {
+        
+        this();
+        
+        this.employeId = id;
+        
+    }
+
+    
+    /**
+     * Retourne le numéro d'identification de l'employé.
+     * 
+     * @return <b>int</b> - Id de l'employé.
+     */
+    public int getId() {
+        
+        return this.employeId;
+        
+    }
+
+    
+    /**
+     * Modification du numéro d'identification de l'employé.
+     * 
+     * @param id - Numéro d'identification de l'employé.
+     */
+    public void setId(int id) {
+        
+        this.employeId = id;
+        
+    }
+
+    
+    /**
+     * Retourne le timesheet à la position <b>index</b> de l'employé.
+     * 
+     * @param index Position de l'employé dans l'index.
+     * @return <b>TimeSheetData</b> - Le timesheet demandé de l'employé.
+     */
+    public TimeSheetData getTimeSheet(int index) {
+        
+        // TODO: throw exception si out of bound!
+        
+        return timesheets.get(index);
+        
+    }
+    
+    
+    // TODO: addTimeSheet(TimeSheetData timesheet)
+    
+    
+    /**
+     * Retourne le nombre de timesheet de l'employé.
+     * 
+     * @return <b>int</b> - Nombre de timesheets.
+     */
+    public int getTimeSheetNum() {
+        
+        return timesheets.size();
+        
+    }  
+    
+    
+    /**
+    * Est-ce que l'employé est un administrateur?
+    *
+    * @return <b>boolean</b> - <b>true</b> si administrateur.
+    */
+    public boolean isAdmin() {
+        
+        return (employeId < 1000);
+        
+    } 
     
 }
