f27edca3f070d2151dacc6cdfb9331508d66f2ec
Thomas Robert de Massy <robert_de_massy.thomas@courrier.uqam.ca>
2014-01-31 02:21:13 -0500

    Squelette du projet (modifications dans le main)

--- src/timesheet/TimeSheet.java
+++ src/timesheet/TimeSheet.java
@@ -20,25 +20,19 @@ import java.util.List;
 
 public class TimeSheet {
 
-    private List<Employe> employees;
-
+    private static List<Employe> employees;
+        
     
-    public TimeSheet() {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
         
         Employe employe = new Employe();
         
         employees = new ArrayList();
         
         employees.add(employe);
-        
-        
-    }
-    
-        
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
 
     }
     
