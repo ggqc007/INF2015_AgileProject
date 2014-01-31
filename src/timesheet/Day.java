23d44b44d5542b8f560f6d3b0864d528ca4ce579
Thomas Robert de Massy <robert_de_massy.thomas@courrier.uqam.ca>
2014-01-31 14:55:06 -0500

    Squelette du projet (modifications dans le main)

--- src/timesheet/Day.java
+++ src/timesheet/Day.java
@@ -20,6 +20,7 @@ import java.util.List;
 
 public class Day {
 
+    private String           name;
     private final List<Task> tasks;
         
     
@@ -27,6 +28,47 @@ public class Day {
         
         tasks = new ArrayList();
         
+    }       
+        
+        
+    public int getTasksNum() {
+        
+        return tasks.size();
+        
     }
+        
+            
+    public String getName() {
+        
+        return this.name;
+        
+    }       
+        
+            
+    public void setName(String name) {
+        
+        this.name = name;
+        
+    }   
+                
+        
+    public void addTask(int id, int time) {
+        
+        Task task = new Task(id, time); 
+        
+        tasks.add(task);
+        
+    }  
+                
+        
+    public Task getTask(int index) {
+            
+        // if index >= getTasksNum() throw exception
+            
+        return tasks.get(index);
+                    
+    }
+        
+    // public boolean isWorkingDay()             
     
 }
