de47e561f70e5f1c135f238aba9dc0e520562461
Guillaume Gagnon <ggqc007@gmail.com>
2014-02-19 21:13:43 -0500

    Refactoring FileReader et FileWriter

--- src/timesheet/FileReader.java
+++ src/timesheet/FileReader.java
@@ -20,22 +20,21 @@ import org.apache.commons.io.IOUtils;
 public class FileReader {
 
     private static final String FILE_ENCODING = "UTF-8";
+    private static String JSONStringFromFile = "";
 
     private static String loadFileIntoString(String filePath, String fileEncoding) throws FileNotFoundException, IOException {
         return IOUtils.toString(new FileInputStream(filePath), fileEncoding);
     }
 
     public static String readJSONFile(String fileInPath) {
-        String JSONText = "";
-
         try {
-            JSONText = FileReader.loadFileIntoString(fileInPath, FILE_ENCODING);
+            JSONStringFromFile = FileReader.loadFileIntoString(fileInPath, FILE_ENCODING);
         } catch (FileNotFoundException e) {
             System.out.println(e);
         } catch (IOException e) {
             System.out.println(e);
         }
-        return JSONText;
+        return JSONStringFromFile;
     }
 
 }
--- src/timesheet/FileWriter.java
+++ src/timesheet/FileWriter.java
@@ -19,6 +19,7 @@ import org.apache.commons.io.IOUtils;
 
 public class FileWriter {
     private static final String FILE_ENCODING = "UTF-8";
+    private static final int JSON_OUTPUT_IDENTATION = 2;
 
     private static void writeStringIntoFile(String data, String unFile, String encoding) throws IOException {
         IOUtils.write(data, new FileOutputStream(unFile), encoding);
@@ -26,7 +27,7 @@ public class FileWriter {
 
     public static void writeJSONFile(JSONArray jsonArray, String fileOutPath) {
         try {
-            FileWriter.writeStringIntoFile(jsonArray.toString(2), fileOutPath, FILE_ENCODING);
+            FileWriter.writeStringIntoFile(jsonArray.toString(JSON_OUTPUT_IDENTATION), fileOutPath, FILE_ENCODING);
         } catch (IOException e) {
             System.out.println(e);
         }
