664b42247d1ac551b00c166095bd0822d4820c19
Guillaume Gagnon <ggqc007@gmail.com>
2014-02-19 20:04:03 -0500

    Modification JSONParser pour respect des 10 lignes par methodes

--- src/timesheet/JSONParser.java
+++ src/timesheet/JSONParser.java
@@ -17,23 +17,15 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 public class JSONParser {
+
     private static final String NOM_CHAMP_NUMERO_EMPLOYE = "numero_employe";
     private static final String NOM_CHAMP_PROJET = "projet";
     private static final String NOM_CHAMP_MINUTES = "minutes";
 
     public static TimeSheetData toTimeSheetData(JSONObject jsonObjectFromFile) {
         TimeSheetData timeSheetData = new TimeSheetData();
-        JSONArray jsonKeysFromFile = jsonObjectFromFile.names();
-
-        for (int i = 0; i < jsonKeysFromFile.size(); i++) {
-            if (jsonKeysFromFile.get(i).equals(NOM_CHAMP_NUMERO_EMPLOYE)) {
-                timeSheetData.setEmployeId(jsonObjectFromFile.getInt(jsonKeysFromFile.getString(i)));
-            } else {
-                Day aDay = timeSheetData.setDayByName(new Day(jsonKeysFromFile.getString(i)));
-                JSONArray taskForADay = jsonObjectFromFile.getJSONArray(jsonKeysFromFile.getString(i));
-                taskToADayIfAny(aDay, taskForADay);
-            }
-        }
+        employeIdToTimeSheetData(timeSheetData, jsonObjectFromFile);
+        createDaysWithTasksInTimeSheetData(timeSheetData, jsonObjectFromFile);
         return timeSheetData;
     }
 
@@ -45,7 +37,27 @@ public class JSONParser {
         return jsonErrorReport;
     }
 
-    private static void taskToADayIfAny(Day aDay, JSONArray taskForADay) {
+    private static void employeIdToTimeSheetData(TimeSheetData timeSheetData, JSONObject jsonObjectFromFile) {
+        JSONArray jsonKeysFromFile = jsonObjectFromFile.names();
+        for (int i = 0; i < jsonKeysFromFile.size(); i++) {
+            if (jsonKeysFromFile.get(i).equals(NOM_CHAMP_NUMERO_EMPLOYE)) {
+                timeSheetData.setEmployeId(jsonObjectFromFile.getInt(jsonKeysFromFile.getString(i))); 
+            }
+        }
+    }
+
+    private static void createDaysWithTasksInTimeSheetData(TimeSheetData timeSheetData, JSONObject jsonObjectFromFile) {
+        JSONArray jsonKeysFromFile = jsonObjectFromFile.names();
+        for (int i = 0; i < jsonKeysFromFile.size(); i++) {
+            if (!jsonKeysFromFile.get(i).equals(NOM_CHAMP_NUMERO_EMPLOYE)) {
+                Day aDay = timeSheetData.setDayByName(new Day(jsonKeysFromFile.getString(i)));
+                JSONArray taskForADay = jsonObjectFromFile.getJSONArray(jsonKeysFromFile.getString(i));
+                tasksToADayIfAny(aDay, taskForADay);
+            }
+        }
+    }
+    
+    private static void tasksToADayIfAny(Day aDay, JSONArray taskForADay) {
         if (!taskForADay.isEmpty()) {
             for (int i = 0; i < taskForADay.size(); i++) {
                 JSONObject taskObj = taskForADay.getJSONObject(i);
