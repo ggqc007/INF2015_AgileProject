2293c63336f3990b19d32258a4a5ecb68d1702a6
Christian Cornejo <cornejo.christian@courrier.uqam.ca>
2014-02-26 22:13:05 -0500

    Refactor Rules, RulesAdmins et RulesEmployes.

--- src/timesheet/Rules.java
+++ src/timesheet/Rules.java
@@ -12,6 +12,7 @@
  */
 
 package timesheet;
+import java.util.ArrayList;
 import java.util.List;
 
 abstract public class Rules {
@@ -20,73 +21,83 @@ abstract public class Rules {
     protected int minOfficeWeekMinutes;
     protected int maxRemoteWeekMinutes;
     protected int minOfficeDailyMinutes;
-    protected int totalWeekMinutes;
-    protected int totalOfficeWeekMinutes;
-    protected int totalRemoteWeekMinutes;
+    protected int totalWeekMinutes = 0;
+    protected int totalOfficeWeekMinutes = 0;
+    protected int totalRemoteWeekMinutes = 0;
     protected Employe employe;
     
     public Rules() {
         maxOfficeWeekMinutes = 43*60;
     }
     
-    abstract public boolean hasMinimumWeeklyTimeInOffice();
-    
-    abstract public  List<Day> getInvalidDaysWithMinimumDailyTimeInOffice();
-    
     abstract public boolean hasValidWeeklyTimeRemote();
     
-    abstract public boolean hasValidWeeklyTimeInOffice();
+    public boolean hasValidWeeklyTimeInOffice() {
+        // Aucun employé n'a le droit de passer plus de 43 heures au bureau.
+        int officeWeekMinutes = totalWeekMinutes - totalRemoteWeekMinutes;
+        return (officeWeekMinutes <= maxOfficeWeekMinutes);
+    }
+    
+    public boolean hasMinimumWeeklyTimeInOffice() {
+        // Les employés normaux doivent travailler au moins 38 heures au bureau par semaine
+        // (excluant le télétravail).
+        int officeWeekMinutes = totalWeekMinutes - totalRemoteWeekMinutes;        
+        return (officeWeekMinutes >= minOfficeWeekMinutes);
+    }    
+    
+    public List<Day> getInvalidDaysWithMinimumDailyTimeInOffice(){
+        // Les employés normaux doivent faire un minimum de 6 heures au bureau pour les jours
+        // ouvrables (lundi au vendredi). 
+        List<Day> invalidDays = new ArrayList(); 
+        List<Day> days = employe.getTimeSheet(0).getDays();       
+        for (int i = 0; i < days.size(); i++)    
+            if (days.get(i).isWorkingDay() == true && getTotalOfficeMinutesByDay(days.get(i)) < minOfficeDailyMinutes) 
+                invalidDays.add(days.get(i));
+       return invalidDays;
+    }    
     
     public void prepData(){
-        setTotalOfficeWeekMinutes();
-        setTotalRemoteWeekMinutes();
-        setTotalWeekMinutes(totalOfficeWeekMinutes + totalRemoteWeekMinutes);
+        calculateTotalWeekMinutes();
     }
     
-    public void setTotalOfficeWeekMinutes() {
-        int totalMinutes = 0;
+    protected void calculateTotalWeekMinutes() {
         List<Day> days = employe.getTimeSheet(0).getDays();       
-        for (int i = 0; i < days.size(); i++) {    
-            totalMinutes += getTotalOfficeMinutesByDay(days.get(i));  
-        }   
-        totalOfficeWeekMinutes = totalMinutes;
+        for (int i = 0; i < days.size(); i++)     
+            calculateTotalWeekMinutesByTasks(days.get(i).getTasks());       
     }
- 
+    
+    protected void calculateTotalWeekMinutesByTasks(List<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) 
+            sumTotalMinutes(tasks.get(i));
+    }
+    
+    protected void sumTotalMinutes(Task task) {
+        int minutes = (int)task.getTime();
+        totalWeekMinutes += minutes;
+        if (task.isRemoteTask()) 
+            totalRemoteWeekMinutes += minutes;
+        else 
+            totalOfficeWeekMinutes += minutes;        
+    }
+
     protected int getTotalOfficeMinutesByDay(Day day) {
         int totalMinutes = 0;
         List<Task> tasks = day.getTasks();
-        for (int i = 0; i < tasks.size(); i++) {
-            if (!tasks.get(i).isRemoteTask()) {
+        for (int i = 0; i < tasks.size(); i++) 
+            if (!tasks.get(i).isRemoteTask()) 
                 totalMinutes += (int)tasks.get(i).getTime();
-            }
-        }
         return totalMinutes;
     }
-    
-    public void setTotalRemoteWeekMinutes() {
-        int totalMinutes = 0;
-        List<Day> days = employe.getTimeSheet(0).getDays();       
-        for (int i = 0; i < days.size(); i++) {  
-            totalMinutes += getTotalRemoteMinutesByDay(days.get(i));
-        }   
-        totalRemoteWeekMinutes = totalMinutes;
-    }    
-    
+
     protected int getTotalRemoteMinutesByDay(Day day) {
         int totalMinutes = 0;
         List<Task> tasks = day.getTasks();
-        for (int i = 0; i < tasks.size(); i++) {
-            if (tasks.get(i).isRemoteTask()) {
-                totalMinutes += (int)tasks.get(i).getTime();
-            }
-        }
+        for (int i = 0; i < tasks.size(); i++) 
+            if (tasks.get(i).isRemoteTask()) 
+                totalMinutes += (int)tasks.get(i).getTime();        
         return totalMinutes;
     }
     
-    public Employe getEmploye() {
-        return employe;
-    }
-
     public void setEmploye(Employe employe) {
         this.employe = employe;
     }
@@ -95,55 +106,27 @@ abstract public class Rules {
         return maxOfficeWeekMinutes;
     }
 
-    public void setMaxOfficeWeekMinutes(int maxOfficeWeekMinutes) {
-        this.maxOfficeWeekMinutes = maxOfficeWeekMinutes;
-    }
-
     public int getMinOfficeWeekMinutes() {
         return minOfficeWeekMinutes;
-    }
-
-    public void setMinOfficeWeekMinutes(int minOfficeWeekMinutes) {
-        this.minOfficeWeekMinutes = minOfficeWeekMinutes;
     }
 
     public int getMaxRemoteWeekMinutes() {
         return maxRemoteWeekMinutes;
     }
 
-    public void setMaxRemoteWeekMinutes(int maxRemoteWeekMinutes) {
-        this.maxRemoteWeekMinutes = maxRemoteWeekMinutes;
-    }
-
     public int getMinOfficeDailyMinutes() {
         return minOfficeDailyMinutes;
-    }
-
-    public void setMinOfficeDailyMinutes(int minOfficeDailyMinutes) {
-        this.minOfficeDailyMinutes = minOfficeDailyMinutes;
     }
 
     public int getTotalWeekMinutes() {
         return totalWeekMinutes;
     }
 
-    public void setTotalWeekMinutes(int totalWeekMinutes) {
-        this.totalWeekMinutes = totalWeekMinutes;
-    }
-
     public int getTotalOfficeWeekMinutes() {
         return totalOfficeWeekMinutes;
     }
 
-    public void setTotalOfficeWeekMinutes(int totalOfficeWeekMinutes) {
-        this.totalOfficeWeekMinutes = totalOfficeWeekMinutes;
-    }
-
     public int getTotalRemoteWeekMinutes() {
         return totalRemoteWeekMinutes;
-    }
-
-    public void setTotalRemoteWeekMinutes(int totalRemoteWeekMinutes) {
-        this.totalRemoteWeekMinutes = totalRemoteWeekMinutes;
     }
 }
