eaa060ee5041adf180c8bb3ad3fd3b0a0c8148af
Guillaume Gagnon <ggqc007@gmail.com>
2014-03-04 21:26:59 -0500

    Efface les entetes

--- src/timesheet/FileWriter.java
+++ src/timesheet/FileWriter.java
@@ -1,15 +1,3 @@
-/* INF2015 – Développement de logiciels dans un environnement Agile
- *
- * Projet de session - Hiver 2014
- *
- * Equipe 8 :
- *
- *   Christian Cornejo
- *   Khaled Elsheikh
- *   Guillaume Gagnon
- *   Thomas Robert de Massy
- *
- */
 package timesheet;
 
 import java.io.FileOutputStream;
--- src/timesheet/RulesAdmins.java
+++ src/timesheet/RulesAdmins.java
@@ -1,16 +1,3 @@
-/* INF2015 – Développement de logiciels dans un environnement Agile
- *
- * Projet de session - Hiver 2014
- *
- * Equipe 8 :
- *
- *   Christian Cornejo
- *   Khaled Elsheikh
- *   Guillaume Gagnon
- *   Thomas Robert de Massy
- *
- */
-
 package timesheet;
 
 public class RulesAdmins extends Rules {
