259afa9edc91905c4f67555c748808109f3e02ab
Christian Cornejo <cornejo.christian@courrier.uqam.ca>
2014-03-05 21:41:34 -0500

    Correction dans le constructer de RulesProduction pour ajouter la méthode manquante calculateTotalWeekMinutes().

--- src/timesheet/RulesProduction.java
+++ src/timesheet/RulesProduction.java
@@ -1,11 +1,12 @@
 package timesheet;
 
-public class RulesProduction  extends Rules {
+public class RulesProduction extends Rules {
 
     public RulesProduction(Employe employe) {
         minOfficeWeekMinutes = 38*60;
         minOfficeDailyMinutes = 6*60; 
         setEmploye(employe);
+        calculateTotalWeekMinutes();
     }
 
     @Override
