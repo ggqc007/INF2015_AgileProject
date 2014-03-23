1b8836fb7a4855a61e03bd10ec7c96aa5712f231
khaled Elsheikh <khaled_elshikh@yahoo.com>
2014-03-23 16:53:54 -0400

    les employe d'administration doivent travailler au min 37.5 et au max 41.5

--- src/timesheet/RulesAdmins.java
+++ src/timesheet/RulesAdmins.java
@@ -3,9 +3,10 @@ package timesheet;
 public class RulesAdmins extends Rules {
 
     public RulesAdmins(Employe employe) {
-        minOfficeWeekMinutes = 36*60;
+        minOfficeWeekMinutes = 37.5*60;
         minOfficeDailyMinutes = 4*60; 
         maxRemoteWeekMinutes = 10*60;
+        maxOfficeWeekMinutes = 41.5*60;
         setEmploye(employe);
         calculateTotalWeekMinutes();
     }
