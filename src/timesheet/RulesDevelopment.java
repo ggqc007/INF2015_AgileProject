f73ae054fe5d906e68b87cd3bc77f04277b9ac29
Thomas Robert de Massy <robert_de_massy.thomas@courrier.uqam.ca>
2014-03-23 22:29:38 -0400

    Refactor RulesProduction -> RulesDevelopment

---
+++ src/timesheet/RulesDevelopment.java
@@ -0,0 +1,16 @@
+package timesheet;
+
+public class RulesDevelopment extends Rules {
+
+    public RulesDevelopment(Employe employe) {
+        minOfficeWeekMinutes = 2280;
+        minOfficeDailyMinutes = 360; 
+        setEmploye(employe);
+        calculateTotalWeekMinutes();
+    }
+
+    @Override
+    public boolean hasValidWeeklyTimeRemote(){ 
+        return true;
+    }
+}
--- src/timesheet/RulesFactory.java
+++ src/timesheet/RulesFactory.java
@@ -8,7 +8,9 @@ public class RulesFactory {
         else if(employe.isExplEmploye())
             return new RulesExploitation(employe);
         else if(employe.isDevelEmploye())
-            return new RulesDirection(employe);
+            return new RulesDevelopment(employe);
+        else if(employe.isDirectionEmploye())
+            return new RulesDirection(employe);        
         return (Rules) new Object();
     }
 }
