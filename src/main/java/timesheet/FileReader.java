59fd100b972ac90896490026edfb9787e43b59b5
Guillaume Gagnon <ggqc007@gmail.com>
2014-04-03 11:30:24 -0400

    Refactor FileReader

--- src/main/java/timesheet/FileReader.java
+++ src/main/java/timesheet/FileReader.java
@@ -10,11 +10,6 @@ public class FileReader {
     private static final String FILE_ENCODING = "UTF-8";
     private static String JSONStringFromFile = "";
 
-    protected static String loadFileIntoString(String filePath, String fileEncoding) 
-            throws FileNotFoundException, IOException {
-        return IOUtils.toString(new FileInputStream(filePath), fileEncoding);
-    }
-
     public static String readJSONFile(String fileInPath) {
         try {
             JSONStringFromFile = FileReader.loadFileIntoString(fileInPath, FILE_ENCODING);
@@ -24,5 +19,10 @@ public class FileReader {
             System.out.println(e);
         }
         return JSONStringFromFile;
+    }
+
+    protected static String loadFileIntoString(String filePath, String fileEncoding)
+            throws FileNotFoundException, IOException {
+        return IOUtils.toString(new FileInputStream(filePath), fileEncoding);
     }
 }
