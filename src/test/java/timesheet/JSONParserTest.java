d17195fa91569e9b5a8b2533d49d2053c8c9c934
Guillaume Gagnon <ggqc007@gmail.com>
2014-04-04 19:06:15 -0400

    Refector final de JSONParserTest

--- src/test/java/timesheet/JSONParserTest.java
+++ src/test/java/timesheet/JSONParserTest.java
@@ -16,8 +16,18 @@ public class JSONParserTest {
     
     @Before
     public void initObjects() {
-        expectedTimeSheet = "TimeSheetData{employeId: 5001, days: [Day{name: \"weekend2\", tasks:[Task{projectId: 990, time: 30}]}, Day{name: \"jour1\", tasks:[Task{projectId: 998, time: 480}, Task{projectId: 911, time: 36}, Task{projectId: 910, time: 8}]}, Day{name: \"jour2\", tasks:[Task{projectId: 125, time: 552}]}, Day{name: \"jour3\", tasks:[Task{projectId: 996, time: 80}]}, Day{name: \"jour4\", tasks:[Task{projectId: 996, time: 80}]}, Day{name: \"jour5\", tasks:[Task{projectId: 125, time: 516}]}, Day{name: \"weekend1\", tasks:[]}]}";
-        validJSONString = "{\n \"numero_employe\": 5001,\n \"jour1\": [\n {\n \"projet\": 998,\n \"minutes\": 480\n },\n {\n \"projet\": 911,\n \"minutes\": 36\n },\n {\n \"projet\": 910,\n \"minutes\": 8\n }\n ],\n \"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n \"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\": [],\n \"weekend2\": [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
+        expectedTimeSheet = "TimeSheetData{employeId: 5001, days: [Day{name: \"weekend2\", tasks:[Task{projectId: 990,"
+                + " time: 30}]}, Day{name: \"jour1\", tasks:[Task{projectId: 998, time: 480}, Task{projectId: 911,"
+                + " time: 36}, Task{projectId: 910, time: 8}]}, Day{name: \"jour2\", tasks:[Task{projectId: 125,"
+                + " time: 552}]}, Day{name: \"jour3\", tasks:[Task{projectId: 996, time: 80}]}, Day{name: \"jour4\","
+                + " tasks:[Task{projectId: 996, time: 80}]}, Day{name: \"jour5\", tasks:[Task{projectId: 125,"
+                + " time: 516}]}, Day{name: \"weekend1\", tasks:[]}]}";
+        validJSONString = "{\n \"numero_employe\": 5001,\n \"jour1\": [\n {\n \"projet\": 998,\n \"minutes\": 480\n },"
+                + "\n {\n \"projet\": 911,\n \"minutes\": 36\n },\n {\n \"projet\": 910,\n \"minutes\": 8\n }\n ],\n "
+                + "\"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n \"jour3\": [\n {\n \"projet\": 996,"
+                + "\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,\n \"minutes\": 80 }\n ],\n "
+                + "\"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\": [],\n \"weekend2\":"
+                + " [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
         validJSONObject = JSONObject.fromObject(validJSONString);
     }
     
@@ -29,7 +39,11 @@ public class JSONParserTest {
     
     @Test(expected = Exception.class)
     public void testToTimeSheetDataMissingDayTimeSheetGeneration() throws Exception {
-        String missingDayJSONString = "{\n \"numero_employe\": 5001,\n \"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n \"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\": [],\n \"weekend2\": [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
+        String missingDayJSONString = "{\n \"numero_employe\": 5001,\n \"jour2\": [\n {\n \"projet\": 125,\n "
+                + "\"minutes\": 552\n }\n ],\n \"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n "
+                + "\"jour4\": [\n {\n \"projet\": 996,\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n "
+                + "\"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\": [],\n \"weekend2\": [\n {\n "
+                + "\"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
         JSONObject missingDayJSONObject = JSONObject.fromObject(missingDayJSONString);
         String result = JSONParser.toTimeSheetData(missingDayJSONObject).toString();
         assertEquals(expectedTimeSheet, result); // Fail if Exception not thrown.
@@ -39,61 +53,10 @@ public class JSONParserTest {
     public void testReportToJSONArray() {
         List<String> errorReport = new ArrayList<String>();
         JSONArray expArray = new JSONArray();
-        
         for (int i = 1; i < 5; i++) {
             errorReport.add("Error"+i);
             expArray.add("Error"+i);
         }
-        System.out.println("Debug1: "+ errorReport.toString());
-        System.out.println("Debug2: "+ expArray.toString());
         assertEquals(expArray.toString(), JSONParser.reportToJSONArray(errorReport).toString());
-    }
-    
-
-
-    /**
-     * Test of employeIdToTimeSheetData method, of class JSONParser.
-     */
-    /*
-    @Test
-    public void testEmployeIdToTimeSheetData() {
-        System.out.println("employeIdToTimeSheetData");
-        TimeSheetData timeSheetData = null;
-        JSONObject jsonObjectFromFile = null;
-        JSONParser.employeIdToTimeSheetData(timeSheetData, jsonObjectFromFile);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    */
-
-    /**
-     * Test of createDaysWithTasksInTimeSheetData method, of class JSONParser.
-     */
-    /*
-    @Test
-    public void testCreateDaysWithTasksInTimeSheetData() {
-        System.out.println("createDaysWithTasksInTimeSheetData");
-        TimeSheetData timeSheetData = null;
-        JSONObject jsonObjectFromFile = null;
-        JSONParser.createDaysWithTasksInTimeSheetData(timeSheetData, jsonObjectFromFile);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    */
-
-    /**
-     * Test of tasksToADayIfAny method, of class JSONParser.
-     */
-    /*
-    @Test
-    public void testTasksToADayIfAny() {
-        System.out.println("tasksToADayIfAny");
-        Day aDay = null;
-        JSONArray taskForADay = null;
-        JSONParser.tasksToADayIfAny(aDay, taskForADay);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    */
-    
+    } 
 }
