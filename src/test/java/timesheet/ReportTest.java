dfb74912db24119a23b92f3abf84600479fb15a1
Guillaume Gagnon <ggqc007@gmail.com>
2014-04-05 14:40:01 -0400

    Remove garbage dans ReportTest

--- src/test/java/timesheet/ReportTest.java
+++ src/test/java/timesheet/ReportTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package timesheet;
 
 import java.util.ArrayList;
@@ -12,10 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author GG
- */
 public class ReportTest {
 
     private static final int ADMIN_EMPLOYE_ID = 1;
