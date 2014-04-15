4e28f9c9244f26cde0ea9e805e4fbbdd39bae9b9
Guillaume Gagnon <ggqc007@gmail.com>
2014-04-15 13:11:01 -0400

    Refactoring - DayTest, jsonString maintenant global

--- src/test/java/timesheet/DayTest.java
+++ src/test/java/timesheet/DayTest.java
@@ -17,6 +17,13 @@ public class DayTest {
     private static final int HOLIDAY_TIME = 480;
     private static final int PARENTAL_HOLIDAY_TASK_ID = 996;    
     private static final int PARENTAL_HOLIDAY_TIME = 480;  
+    
+    private static final String validJSONStringAdmin = "{\n \"numero_employe\": 1,\n \"jour1BAD\": [\n {\n \"projet\": 998,"
+                + "\n \"minutes\": 480\n },\n {\n \"projet\": 911,\n \"minutes\": 36\n },\n {\n \"projet\": 910,\n "
+                + "\"minutes\": 8\n }\n ],\n \"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n "
+                + "\"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,"
+                + "\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n "
+                + "\"weekend1\": [],\n \"weekend2\": [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
 
     @Before
     public void initObjects() {
@@ -25,12 +32,6 @@ public class DayTest {
     @Test(expected = IllegalArgumentException.class)
     public void testIsValidDayNameFail() throws Exception {
         Employe employeAdmin = new Employe();
-        String validJSONStringAdmin = "{\n \"numero_employe\": 1,\n \"jour1BAD\": [\n {\n \"projet\": 998,"
-                + "\n \"minutes\": 480\n },\n {\n \"projet\": 911,\n \"minutes\": 36\n },\n {\n \"projet\": 910,\n "
-                + "\"minutes\": 8\n }\n ],\n \"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n "
-                + "\"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,"
-                + "\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n "
-                + "\"weekend1\": [],\n \"weekend2\": [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
         JSONObject validJSONObjectAdmin = JSONObject.fromObject(validJSONStringAdmin);
         TimeSheetData validTimeSheetDataAdmin = JSONParser.toTimeSheetData(validJSONObjectAdmin);
         employeAdmin.initFromFirstTimeSheet(validTimeSheetDataAdmin);
@@ -39,12 +40,6 @@ public class DayTest {
     @Test(expected = Exception.class)
     public void testIsWorkingDayFail() throws Exception {
         Employe employeAdmin = new Employe();
-        String validJSONStringAdmin = "{\n \"numero_employe\": 1,\n \"jo\": [\n {\n \"projet\": 998,"
-                + "\n \"minutes\": 480\n },\n {\n \"projet\": 911,\n \"minutes\": 36\n },\n {\n \"projet\": 910,\n "
-                + "\"minutes\": 8\n }\n ],\n \"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n "
-                + "\"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,"
-                + "\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n "
-                + "\"weekend1\": [],\n \"weekend2\": [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
         JSONObject validJSONObjectAdmin = JSONObject.fromObject(validJSONStringAdmin);
         TimeSheetData validTimeSheetDataAdmin = JSONParser.toTimeSheetData(validJSONObjectAdmin);
         employeAdmin.initFromFirstTimeSheet(validTimeSheetDataAdmin);
