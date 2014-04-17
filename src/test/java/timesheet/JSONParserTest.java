0b3e36cfd7a23d288c68311e7d2bc3434ca1103c
Guillaume Gagnon <ggqc007@gmail.com>
2014-04-17 14:02:41 -0400

    Refactoring - JSONParserTest

--- src/test/java/timesheet/JSONParserTest.java
+++ src/test/java/timesheet/JSONParserTest.java
@@ -10,24 +10,28 @@ import static org.junit.Assert.*;
 
 public class JSONParserTest {
     
-    private String expectedTimeSheet;
-    private String validJSONString;
+    private static final String validJSONString = "{\n \"numero_employe\": 5001,\n \"jour1\": [\n {\n \"projet\": 998,"
+                + "\n \"minutes\": 480\n },\n {\n \"projet\": 911,\n \"minutes\": 36\n },\n {\n \"projet\": 910,\n "
+                + "\"minutes\": 8\n }\n ],\n \"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n \"jour3\":"
+                + " [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,\n "
+                + "\"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\":"
+                + " [],\n \"weekend2\": [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
+    private static final String expectedTimeSheet = "TimeSheetData{employeId: 5001, days: [Day{name: \"weekend2\","
+                + " tasks:[Task{projectId: 990, time: 30}]}, Day{name: \"jour1\", tasks:[Task{projectId: 998, time:"
+                + " 480}, Task{projectId: 911, time: 36}, Task{projectId: 910, time: 8}]}, Day{name: \"jour2\", tasks:"
+                + "[Task{projectId: 125, time: 552}]}, Day{name: \"jour3\", tasks:[Task{projectId: 996, time: 80}]}, "
+                + "Day{name: \"jour4\", tasks:[Task{projectId: 996, time: 80}]}, Day{name: \"jour5\", "
+                + "tasks:[Task{projectId: 125, time: 516}]}, Day{name: \"weekend1\", tasks:[]}]}";
+    private static final String missingDayJSONString = "{\n \"numero_employe\": 5001,\n \"jour2\": [\n {\n \"projet\": "
+                + "125,\n \"minutes\": 552\n }\n ],\n \"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n "
+                + "\"jour4\": [\n {\n \"projet\": 996,\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n "
+                + "\"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\": [],\n \"weekend2\": [\n {\n "
+                + "\"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
+    
     private JSONObject validJSONObject;
     
     @Before
     public void initObjects() {
-        expectedTimeSheet = "TimeSheetData{employeId: 5001, days: [Day{name: \"weekend2\", tasks:[Task{projectId: 990,"
-                + " time: 30}]}, Day{name: \"jour1\", tasks:[Task{projectId: 998, time: 480}, Task{projectId: 911,"
-                + " time: 36}, Task{projectId: 910, time: 8}]}, Day{name: \"jour2\", tasks:[Task{projectId: 125,"
-                + " time: 552}]}, Day{name: \"jour3\", tasks:[Task{projectId: 996, time: 80}]}, Day{name: \"jour4\","
-                + " tasks:[Task{projectId: 996, time: 80}]}, Day{name: \"jour5\", tasks:[Task{projectId: 125,"
-                + " time: 516}]}, Day{name: \"weekend1\", tasks:[]}]}";
-        validJSONString = "{\n \"numero_employe\": 5001,\n \"jour1\": [\n {\n \"projet\": 998,\n \"minutes\": 480\n },"
-                + "\n {\n \"projet\": 911,\n \"minutes\": 36\n },\n {\n \"projet\": 910,\n \"minutes\": 8\n }\n ],\n "
-                + "\"jour2\": [\n {\n \"projet\": 125,\n \"minutes\": 552\n }\n ],\n \"jour3\": [\n {\n \"projet\": 996,"
-                + "\n \"minutes\": 80\n }\n ],\n \"jour4\": [\n {\n \"projet\": 996,\n \"minutes\": 80 }\n ],\n "
-                + "\"jour5\": [\n  {\n \"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\": [],\n \"weekend2\":"
-                + " [\n {\n \"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
         validJSONObject = JSONObject.fromObject(validJSONString);
     }
     
@@ -39,14 +43,9 @@ public class JSONParserTest {
     
     @Test(expected = Exception.class)
     public void testToTimeSheetDataMissingDayTimeSheetGeneration() throws Exception {
-        String missingDayJSONString = "{\n \"numero_employe\": 5001,\n \"jour2\": [\n {\n \"projet\": 125,\n "
-                + "\"minutes\": 552\n }\n ],\n \"jour3\": [\n {\n \"projet\": 996,\n \"minutes\": 80\n }\n ],\n "
-                + "\"jour4\": [\n {\n \"projet\": 996,\n \"minutes\": 80 }\n ],\n \"jour5\": [\n  {\n "
-                + "\"projet\": 125,\n \"minutes\": 516 }\n ],\n \"weekend1\": [],\n \"weekend2\": [\n {\n "
-                + "\"projet\": 990,\n \"minutes\": 30\n }\n ]\n}";
         JSONObject missingDayJSONObject = JSONObject.fromObject(missingDayJSONString);
         String result = JSONParser.toTimeSheetData(missingDayJSONObject).toString();
-        assertEquals(expectedTimeSheet, result); // Fail if Exception not thrown.
+        assertEquals(expectedTimeSheet, result);
     }
     
     @Test
