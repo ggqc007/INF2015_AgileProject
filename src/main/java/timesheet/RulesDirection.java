fda874a446691a14b227f4c141abcb667869d64e
Christian Cornejo <cornejo.christian@courrier.uqam.ca>
2014-04-16 22:10:15 -0400

    Correction du temps total au bureau pour direction en tenant compte du temps de transport

--- src/main/java/timesheet/RulesDirection.java
+++ src/main/java/timesheet/RulesDirection.java
@@ -1,5 +1,7 @@
 package timesheet;
 
+import java.util.List;
+
 public class RulesDirection extends Rules {
 
     public RulesDirection(Employe employe) {
@@ -9,6 +11,16 @@ public class RulesDirection extends Rules {
         setEmploye(employe);
         calculateTotalWeekMinutes();
         calculateTotalWeekTransportTime();        
+    }
+    
+    @Override
+    protected int getTotalOfficeMinutesByDay(Day day) {
+        int totalMinutes = 0;
+        List<Task> tasks = day.getTasks();
+        for (int i = 0; i < tasks.size(); i++) 
+            if (!tasks.get(i).isRemoteTask() && !tasks.get(i).isTransportationTask()) 
+                totalMinutes += (int)tasks.get(i).getTime();
+        return totalMinutes;
     }
 
     @Override
